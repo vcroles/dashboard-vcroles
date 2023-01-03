dbe6abf520a8ace95d3f9586a7298e53c2ba5053
CDE90 <ethan@ethancoward.dev>
2023-01-03 13:03:48 +0000

    Make the guild dashboard sidebar etc. a layout for reusability with other pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,30 @@
-import { type AppType } from "next/app";
+import type { AppType, AppProps } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import type { NextPage } from "next";
+import type { ReactElement, ReactNode } from "react";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
-}) => {
+}: AppPropsWithLayout) => {
+    const getLayout = Component.getLayout ?? ((page) => page);
+
     return (
         <SessionProvider session={session}>
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
         </SessionProvider>
     );
 };
