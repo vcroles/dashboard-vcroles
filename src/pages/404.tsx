7318b7c5f7ab0a619d3de9b14ab98e96b75fc1d2
CDE90 <cdeyolo@outlook.com>
2023-01-03 22:26:52 +0000

    Update contact support button on the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -26,12 +26,12 @@ export default function NotFound() {
                                 >
                                     Go back home
                                 </Link>
-                                <a
-                                    href="#"
+                                <Link
+                                    href="https://discord.gg/yHU6qcgNPy"
                                     className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 >
                                     Contact support
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </main>
