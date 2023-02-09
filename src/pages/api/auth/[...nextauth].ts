7754749ba4dade244909070b83cf626339a9e8ba
CDE90 <ethan@ethancoward.dev>
2023-02-09 22:15:37 +0000

    Force users to re-authenticate when session expires

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,10 +8,31 @@ import { authClient } from "../../../server/db/client";
 export const authOptions: NextAuthOptions = {
     // Include user.id on session
     callbacks: {
-        session({ session, user }) {
+        async session({ session, user }) {
             if (session.user) {
                 session.user.id = user.id;
             }
+
+            if (session.user) {
+                const userAccount = await authClient.account.findFirst({
+                    where: {
+                        userId: session.user.id,
+                    },
+                });
+
+                if (Date.now() >= (userAccount?.expires_at ?? 0) * 1000) {
+                    // delete the user
+                    await authClient.user.deleteMany({
+                        where: {
+                            id: session.user.id,
+                        },
+                    });
+
+                    // @ts-expect-error Force the session to be deleted
+                    session = null;
+                }
+            }
+
             return session;
         },
     },
